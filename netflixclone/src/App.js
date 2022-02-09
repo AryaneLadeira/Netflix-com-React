7f93c1ecfba587b620b6a705f4be3ca91ac222eb
Aryane Ladeira <69313233+AryaneLadeira@users.noreply.github.com>
2022-02-09 09:22:31 -0300

    Componente header criado e estilizado

--- netflixclone/src/App.js
+++ netflixclone/src/App.js
@@ -3,11 +3,13 @@ import Tmdb from "./Tmdb";
 import './App.css';
 import MovieRow from "./components/MovieRow";
 import FeaturedMovie from "./components/FeaturedMovie";
+import Header from "./components/Header";
 
 export default () =>{
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false)
 
   useEffect(() => {
     const loadAll = async () => {
@@ -24,13 +26,31 @@ export default () =>{
     loadAll();
   }, []);
 
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10){
+        setBlackHeader(true);
+      }else{
+        setBlackHeader(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener);
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
+
   return (
     <div className="page">
-      {featuredData && <FeaturedMovie item={featuredData}/>}
+      <Header black={blackHeader} />
+
+      {featuredData && <FeaturedMovie item={featuredData} />}
       
       <section className="lists">
       {movieList.map((item, key) => (
-        <MovieRow key={key} title={item.title} items={item.items}/>
+        <MovieRow key={key} title={item.title} items={item.items} />
       ))}
       </section> 
     </div>
